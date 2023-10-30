742481ddc3ef7a98fda26cac164e0d30a907e871
jshipton <j.shipton@exeter.ac.uk>
2023-10-30 20:00:41 +0000

    demo notebook and script

---
+++ functions.py
@@ -0,0 +1,8 @@
+def print_me(input):
+    "This function prints a hello message and then the input string"
+    print("hello from the print_me function")
+    print("this was your input: ", input)
+
+def square(x):
+    " This function has input x and returns x**2"
+    return x**2
